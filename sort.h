ae3a5398c2c132c17929801ce5b3abf4e25468b1
Joseph Mariga <jsphmariga@gmail.com>
2022-07-19 23:50:47 +0000

    Add header file

--- sort.h
+++ sort.h
@@ -22,6 +22,7 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 #endif
